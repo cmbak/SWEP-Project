3871dd8ebffd931b342036b0e675adddd1000b75
ec22697 <ec22697@qmul.ac.uk>
2024-04-03 16:26:50 +0100

    Moved example listing to separate file

--- app/(tabs)/viewAccom.js
+++ app/(tabs)/viewAccom.js
@@ -20,6 +20,7 @@ import translate from 'google-translate-api-x';
 import { I18n } from 'i18n-js';
 import { translations } from '../../localizations';
 import { RandomRatings } from '../../randomRatings';
+import { exampleResult } from '../../exampleListing';
 
 /*
 Accommodation
@@ -36,59 +37,6 @@ Accommodation
 
 /* Could add Scrollable Reviews */
 
-// Just to test this without calling the api
-const exampleResult = {
-    additionalLinks: [],
-    address: 'New Windsor Street, Uxbridge UB8',
-    agentAddress:
-        '6 Odeon Parade, Sudbury Heights Avenue, Greenford, Middlesex, London',
-    agentName: 'Chase Residential',
-    agentPhone: '020 8115 8614',
-    availableFrom: '2022-01-13T00:00:00',
-    baths: 1,
-    beds: null,
-    bedsMax: null,
-    bedsMin: null,
-    category: 'residential',
-    description:
-        'Chase Residential is presenting this, first floor Studio flat to rent within moments from Uxbridge Town Centre.<br><br>Rent includes water rates.<br><br>Council tax and electric bills to be paid by the tenants.<br><br>This Studio comprises of: Fitted kitchen, cubical shower room and good size living/bedroom area.<br><br>It further benefits from, electric heating, double glazed windows, laminate flooring and offered furnished.<br><br>There is the use of a communal Washing Machine &amp; No Parking for this property.<br><br>Council Tax: Band A<br><br>available 13th January 2022.',
-    features: [
-        'Water',
-        'Double glazing',
-        'Wood floors',
-        'Council Tax - Band A',
-        'Energy Rating : D',
-        'Tenants Pays Council Tax &amp; Electricity',
-    ],
-    floorPlan: [],
-    id: '51836428',
-    images: [
-        'https://lid.zoocdn.com/u/2400/1800/4bd77677c54ac6b618647d7a95a0121e9139edef.jpg',
-        'https://lid.zoocdn.com/u/2400/1800/e53e95928d49a6b7698812f588a3d8c379139f4b.jpg',
-        'https://lid.zoocdn.com/u/2400/1800/5e5e3006fda7b2186d4f0b75df2601a637ced2aa.jpg',
-        'https://lid.zoocdn.com/u/2400/1800/5563a79b677d1d69e463290d4ef3e7ee94452c7b.jpg',
-        'https://lid.zoocdn.com/u/2400/1800/66a5c9c24e8136b01de25d0966b1fc28c2ac3238.jpg',
-    ],
-    isRetirementHome: false,
-    isSharedOwnership: false,
-    latitude: 51.54458,
-    listingCondition: 'pre-owned',
-    listingStatus: 'to_rent',
-    livingRooms: 0,
-    longitude: -0.483798,
-    name: 'Studio to rent',
-    postalCode: 'UB8 2TX',
-    price: 825,
-    priceActual: 825,
-    priceMax: 900,
-    priceMin: 800,
-    propertyType: 'studio',
-    publishedOn: '2024-03-19T12:17:29',
-    sqft: '',
-    studentFriendly: false,
-    url: 'https://www.zoopla.co.uk/to-rent/details/51836428/',
-    uuid: 'B8A5A667-DF64-430D-B384-701781C50E9A',
-};
 export default function viewAccom({ propertyId = 51836428 }) {
     const i18n = new I18n(translations);
     const [locale, setLocale] = useState(getLocales()[0].languageCode);
